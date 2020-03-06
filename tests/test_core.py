983fdaff5115429433278ee5dc9298105eafd716
Hiroshi Toyama <toyama0919@gmail.com>
2020-03-06 19:48:15 +0900

    add core test.

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,13 +1,12 @@
 from h5cv.core import Core
 
 
-class TestUtil(object):
+class TestCore(object):
     def setup_method(self, method):
-        pass
+        self.core = Core(hdf5="tmp/test.h5")
 
     def teardown_method(self, method):
         pass
 
-    def test_pad_zero(self):
-        # assert s3.get("2020-03") == 0
-        pass
+    def test_write(self):
+        self.core.write("tests/data/*.jpg")
